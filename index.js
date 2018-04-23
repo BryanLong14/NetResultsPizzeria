c956e387087b95860da8e7ab9e091a9265a5c94e
Bryan Long <BryanLong1@gmail.com>
2018-04-23 15:08:09 -0600

    Finished fetch reqeust and styling

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const API = "https://59b6v76zci.execute-api.us-west-2.amazonaws.com/nr/example";
+const API2 = "https://pure-refuge-52497.herokuapp.com/hole";
+const output = document.getElementById("output");
 
-document.getElementById("get-data").addEventListener("click", getData);
-
-function getData() {
-  console.log("TESTING");
-  fetch(API, {
-    mode: "cors",
-    headers: new Headers({
-      "Content-Type": "application/JSON"
-    })
+fetch(API)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+    return res;
   })
-    .then(res => res.json())
-    .then(res => {
-      console.log(res);
-      let result = "<h2>Pizza Order</h2>";
-      result += `<h3>Order ID: ${data.order_id}</h3>
-        <h4>Size: ${data.size}</h4>`;
-    })
-    .catch(err => console.log(err));
+  .then(displayOrderData)
+  .catch(err => console.log(err));
+
+function displayOrderData(data) {
+  let order = data.order_id;
+  let size = data.size;
+  let crust = data.crust;
+  let toppings = data.toppings.map(topping => {
+    return `<h5>${topping.amount} ${topping.name}</h5>`;
+  });
+  return (output.innerHTML = `
+  <h2>New Pizza Order Number: ${order}</h2>
+  <h2>Size: ${size}</h2>
+  <h2>Crust: ${crust}</h2>
+  <h2>Toppings: ${toppings}</h2>`.replace(/\,/g, ""));
 }
